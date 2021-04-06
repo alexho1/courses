70e8107782adfbc678813597e0937576dafa533d
Alex Ho <mralexho@gmail.com>
2021-04-06 19:47:23 -0400

    config rest framework

--- courses/settings.py
+++ courses/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'courses',
+    'rest_framework',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +40,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': ['rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly']
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
