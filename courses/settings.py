73145464bb194fd88e2f9c28cd2888c72f7e7680
Alex Ho <mralexho@gmail.com>
2021-04-06 19:59:16 -0400

    update db settings

--- courses/settings.py
+++ courses/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'courses.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'courses',
+        'USER': 'courses',
+        'PASSWORD': 'courses',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
     }
 }
 
